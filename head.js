392c051dee40dcccac418c35e5c09d78ae05217d
RajMazing <caseyraj92@gmail.com>
2021-11-23 21:57:05 +0000

    added head function

---
+++ head.js
@@ -0,0 +1,25 @@
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+
+};
+
+
+
+
+let head = function(firstElement) {
+  return firstElement[0];
+
+};
+
+// TEST CODE
+
+
+
+assertEqual(head[5,6,7], 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
