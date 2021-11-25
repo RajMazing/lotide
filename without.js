1bbaba3d8b9ac2ba21b31b44ef6e8cb9505a0ddf
RajMazing <caseyraj92@gmail.com>
2021-11-25 02:57:41 +0000

    function to remove parts of an array

---
+++ without.js
@@ -0,0 +1,50 @@
+
+
+
+
+  const eqArrays = function(arrayOne, arrayTwo) {
+    if (arrayOne.length !== arrayTwo.length) {
+      return false;
+    }
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+    
+  };
+  
+  const assertArraysEqual = function(actual, expected) {
+    if (eqArrays(actual, expected)) {
+      console.log(`It has passed!`);
+    } else {
+      console.log(`It has failed!`);
+    }
+  };
+
+
+ const without = function(source, itemsToRemove) {
+    let arr = [];
+    for(let i = 0; i < source.length; i++){
+      if(source[i] !== itemsToRemove[i]){
+        arr.push(source[i]);
+      }
+    }
+    return arr;
+    }
+
+
+ 
+
+
+
+//  console.log(without([1, 2, 3], [1])) // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"]))// => ["1", "2"]
+
+
+
+  const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
