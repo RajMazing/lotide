138250c0eb206a0f30c0445a4783c18e524ef231
RajMazing <caseyraj92@gmail.com>
2021-11-29 23:25:34 +0000

    code updated and moved to test file of same name

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,19 +1,19 @@
+const eqArrays = require('./eqArray')
 
 
 
-
-  const eqArrays = function(arrayOne, arrayTwo) {
-    if (arrayOne.length !== arrayTwo.length) {
-      return false;
-    }
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-    return true;
+  // const eqArrays = function(arrayOne, arrayTwo) {
+  //   if (arrayOne.length !== arrayTwo.length) {
+  //     return false;
+  //   }
+  //   for (let i = 0; i < arrayOne.length; i++) {
+  //     if (arrayOne[i] !== arrayTwo[i]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
     
-  };
+  // };
   
   const assertArraysEqual = function(actual, expected) {
     if (eqArrays(actual, expected)) {
@@ -24,4 +24,6 @@
   };
 
 
-  assertArraysEqual([1, 2, 3], [1, 2, 3]);
+  
+
+  module.exports = assertArraysEqual;
