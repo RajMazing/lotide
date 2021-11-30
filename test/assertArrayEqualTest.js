7e1965fa636aa0263e74454839ab06b0c0c6a14a
RajMazing <caseyraj92@gmail.com>
2021-11-30 03:33:08 +0000

    updated code to pass test

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,7 +1,18 @@
-const assertArraysEqual = require('../assertArrayEqual')
+// const assertArraysEqual = require('../assertArrayEqual')
 
 
 
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+
+const assertArraysEqual = require('../assertArrayEqual');
+const assert = require('chai').assert
+
+describe("#assertArrayEqual", () => {
+    it ("returns They are the same. for [1, 2, 3], [1, 2, 3]", () => {
+        assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+    });
+    
+});
